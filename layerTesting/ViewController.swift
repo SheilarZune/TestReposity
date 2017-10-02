bc547b16af900040edb2ccf3b50674254100c2f1
Sheilar June <Sheilar@sheilarjune.local>
2017-10-02 16:25:32 +0630

    Initial Commit

---
+++ layerTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  layerTesting
+//
+//  Created by Sheilar June on 10/2/17.
+//  Copyright © 2017 Sheilar June. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
